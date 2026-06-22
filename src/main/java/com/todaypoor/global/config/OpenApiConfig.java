3201e14970e6fb7d13f0ca39c49daa1bc48170c5
sinyeowon <ioohyou@knu.ac.kr>
2026-06-23 04:48:04 +0900

    config(auth): OpenApiConfig 추가 및 JWT Bearer 테스트 설정

---
+++ src/main/java/com/todaypoor/global/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.todaypoor.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("오늘거지 (TodayPoor) API Specification")
+                .description("오늘거지 서비스의 백엔드 API 명세서입니다.")
+                .version("v1.0.0");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(SECURITY_SCHEME_NAME);
+
+        Components components = new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityRequirementsItem(securityRequirement)
+                .components(components);
+    }
+}
