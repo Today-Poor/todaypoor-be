2be3847a118af1b2dbaf282a4c91759ab0a3a852
sinyeowon <ioohyou@knu.ac.kr>
2026-06-23 05:06:15 +0900

    docs(swagger): Swagger API 문서 타이틀 및 서비스 설명 수정

--- src/main/java/com/todaypoor/global/config/OpenApiConfig.java
+++ src/main/java/com/todaypoor/global/config/OpenApiConfig.java
@@ -1,12 +1,13 @@
 package com.todaypoor.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,8 +17,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("오늘거지 (TodayPoor) API Specification")
-                .description("오늘거지 서비스의 백엔드 API 명세서입니다.")
+                .title("오늘의 거지 (TodayPoor) API Specification")
+                .description("오늘의 거지 서비스의 백엔드 API 명세서입니다.")
                 .version("v1.0.0");
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
